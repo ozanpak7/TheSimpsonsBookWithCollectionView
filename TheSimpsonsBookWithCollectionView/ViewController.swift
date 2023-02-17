bc6f7e6a8b1009d558062b83dc7b886c979b1983
Ozan Pak <ozan.pak@ibtech.com.tr>
2023-02-17 13:42:08 +0300

    Initial Commit

---
+++ TheSimpsonsBookWithCollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheSimpsonsBookWithCollectionView
+//
+//  Created by T51470 on 17.02.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
